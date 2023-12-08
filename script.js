7e2129735f6dc7208f99f0abf39fa2eb48ed0070
farisubuntu <farisubuntu@gmail.com>
2023-12-09 00:46:18 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,159 @@
+onload = function() {
+    caller(url_all, "a");
+};
+// global Objects:
+var all;
+var category; // such 1.json,36.json,......
+var vocabularies; //such as ...../vocabularies/201.json,....
+var lesson; // such as ...../id/203.json,...
+var quizzes; // later uses
+var lesson_urls;
+// sample full url for an audio file=> https://d13tz37rv54ob.cloudfront.net/ur/TnBUFMy7uqu_Rwilf-UzaSzeyNFwNZFm?t=1688826351
+var url_all =
+    "https://raw.githubusercontent.com/farisubuntu/urdu-course/gh-pages/data/all.json";
+
+// data_obj;
+// category; // 1,2,...,88,.....
+// lesson; // lesson object inside files 101,102,103....lesson
+// quizzes; // quizzes array inside file 101,102,.......quizzes[quiz_no]
+// all; // always 'all'
+
+async function getJSON(url) {
+    return fetch(url).then(response => response.json()).then(responseJson => {
+        return responseJson;
+    });
+}
+async function caller(url, obj_type) {
+    const json = await this.getJSON(url); // command waits until completion
+    // data is now available
+    if (obj_type == "a") {
+        // all.json
+        all = json.data;
+        console.log("all: ", all);
+        appendCardsContent();
+    }
+    if (obj_type == "v") {
+        // vocabularies
+        vocabularies = json;
+        console.log("vocabularies", vocabularies);
+        appendVocabularies();
+    }
+    if (obj_type == "c") {
+        category = json;
+        console.log("obj_type=l, so", category);
+        get_top_bar();
+    }
+    if (obj_type == "l") {
+        // such 201.json,.....
+        lesson = json.lesson;
+        quizzes = json.quizzes;
+        console.log("lesson_obj", lesson);
+        console.log("quizzes obj", quizzes);
+        appendLessonData();
+    } else {
+        console.log("object type unknowen....");
+    }
+}
+
+function appendCardsContent() {
+    var html = `<div class="cards">`;
+    for (var index = 0; index < all.length; index++) {
+        html += get_card_category(all[index].category);
+    }
+    html += "</div>";
+    document.querySelector(".content-wrapper").innerHTML = html;
+    // add cards events:
+    document.querySelectorAll(".card").forEach(function f(elem) {
+        elem.addEventListener("click", showLessonContent);
+    });
+}
+
+function showLessonContent(target) {
+    console.log(target.currentTarget); //always get the .card element
+
+    let id = target.currentTarget.getAttribute("id");
+    lesson_urls = makeUrls(id);
+
+    // fetch category and append top bar:
+    caller(lesson_urls[0], "c"); // now cat has the same data as global 'category' has the data
+    caller(lesson_urls[7], "v");
+    // empty content-wrapper area:
+    document.querySelector(".content-wrapper").innerHTML = "";
+}
+
+// vocabularies fiels:
+// https://raw.githubusercontent.com/farisubuntu/urdu-course/gh-pages/data/....
+
+function makeUrls(id) {
+    let urls = [];
+    // add 'id'
+    let const_portion =
+        "https://raw.githubusercontent.com/farisubuntu/urdu-course/gh-pages/data/";
+    console.log(const_portion);
+    // append 'id'+'.json' (lesson file) as the zero index item
+    var category_file = const_portion + id + "/" + id + ".json";
+    console.log("category file url=", category_file);
+    urls[0] = category_file; //category url
+    // append lessons urls
+    for (var i = 1; i < 7; i++) {
+        urls[i] = const_portion + id + "/" + id + "0" + i.toString() + ".json";
+    }
+    // append seventh item -- const_portion+id/vocabularies/id01.json'
+    urls[7] = const_portion + id + "/vocabularies/" + id + "01.json";
+    return urls;
+}
+
+// sample audio link:
+//+ https://d13tz37rv54ob.cloudfront.net/ur/5yJIklmE-tT6L44G-d2no9epgjQXY1kV?t=1580691716
+//+ 'https://d13tz37rv54ob.cloudfront.net/ur/' +
+// sample image link (phrase-image): 'https://d37sy4vufic209.cloudfront.net/phrase-images/4LxKr9YvDJgNMNCsfBNDDmpo1QDNYJsM'
+function openLesson(e, id) {
+    console.log('openLesson(e,id).....');
+    console.log(
+        "button pressed=> ",
+        e.currentTarget,
+        " ..... and id=> ",
+        id.valueOf()
+    );
+    //  keep this button as pressed:
+    e.currentTarget.style.backgroundColor = "#106DBD";
+    e.currentTarget.style.color = "white";
+    // keep other buttons as initial state
+    resetButtons(id);
+
+    let cat_no, lesson_no, url;
+    if (id.length == 3) {
+        console.log('id.length==3')
+        cat_no = id.valueOf()[0];
+        lesson_no = cat_no + id.valueOf()[1] + id.valueOf()[2];
+        console.log('cat_no = ', cat_no, ' and lesson_no= ', lesson_no);
+    }
+    if (id.length == 4) {
+        console.log('id.length==4')
+        cat_no = id.valueOf()[0] + id.valueOf()[1];
+        lesson_no = cat_no + id.valueOf()[2] + id.valueOf()[3];
+        console.log('cat_no = ', cat_no, ' and lesson_no= ', lesson_no);
+    }
+
+    url = `https://raw.githubusercontent.com/farisubuntu/urdu-course/gh-pages/data/${cat_no}/${lesson_no}.json`;
+    console.log(`openLesson(): now call caller(${url},'l')`);
+    caller(url, "l");
+}
+
+function appendLessonData() {
+    console.log("appendLessonData()...all..");
+    get_word_template_table();
+}
+
+function appendVocabularies() {
+    console.log("appendVocabularies()......");
+}
+
+function resetButtons(id) {
+    document.querySelectorAll(".tablinks").forEach(function f(btn) {
+        if (btn.getAttribute("id") !== id) {
+            btn.style.backgroundColor = "initial";
+            btn.style.color = "initial";
+        }
+    });
+}
---
+++ templates.js
@@ -0,0 +1,148 @@
+function get_card_category(cat) {
+    var card = `
+  <div class="card" id="${cat.id}">
+   <div img_wrapper><img src="images/${cat.id}.jpeg" alt="Avatar" /></div>
+   <div class="container">
+    <p><b>${cat.name}</b></p>
+    <p class="cat-id">${cat.id}</p>
+   </div>
+ </div>
+`;
+    return card;
+}
+
+function get_vocabularies_template(voc) {
+    console.log("get_vocabularies_template()...");
+}
+
+function get_top_bar() {
+    var html = `
+ <div class="tab">
+  <button id="${category.lessons[0]
+      .id}" class="tablinks" onclick="openLesson(event, '${category.lessons[0]
+        .id}')">${category.lessons[0].name}</button>
+  <button id="${category.lessons[1]
+      .id}" class="tablinks" onclick="openLesson(event, '${category.lessons[1]
+        .id}')">${category.lessons[1].name}</button>
+  <button id="${category.lessons[2]
+      .id}" class="tablinks" onclick="openLesson(event, '${category.lessons[2]
+        .id}')">${category.lessons[2].name}</button>
+  <button id="${category.lessons[3]
+      .id}" class="tablinks" onclick="openLesson(event, '${category.lessons[3]
+        .id}')">${category.lessons[3].name}</button>
+  <button id="${category.lessons[4]
+      .id}" class="tablinks" onclick="openLesson(event, '${category.lessons[4]
+        .id}')">${category.lessons[4].name}</button>
+  <button id="${category.lessons[5]
+      .id}" class="tablinks" onclick="openLesson(event, '${category.lessons[5]
+        .id}')">${category.lessons[5].name}</button>
+</div>
+ `;
+    document.querySelector(".nav-header").innerHTML = html;
+}
+//  Global Constant:
+var const_audio_portion = "https://d13tz37rv54ob.cloudfront.net/ur/";
+var const_image_portion = "https://d37sy4vufic209.cloudfront.net/phrase-images/";
+// appendPhrasesData()
+function appendPhrasesData() {
+    //-------------- append phrases/examples data: --------
+    // var phrases_index = [1, 3, 4, 5, 7, 8];
+    console.log('appendPhrasesData()....');
+    var phrases = ``;
+    var row = ``;
+    for (var i = 0; i < quizzes.length; i++) {
+        if ("alts" in quizzes[i]) {
+            if (quizzes[i].alts.length == 3) {
+                for (var k = 0; k < 3; k++) {
+                    row = `
+    <div class="row">
+     <div class="cell index" width="20px">${i} - ${quizzes[i].type}</div>
+     <div class="cell target">${quizzes[i].alts[k].text}</div>
+     <div class="cell mother">${quizzes[i].sols[k].text}</div>
+     <div class="cell audio">
+        <i class="fa fa-play"></i><audio preload="metadata"  src="${const_audio_portion +
+            quizzes[i].sols[k].key +
+            "?t=" +
+            quizzes[i].sols[k]
+              .audio_updated_at}" type='audio/mp3'>Your browser not supported
+        </audio>
+     </div>
+    </div>
+   `;
+                    phrases += row;
+                }
+            }
+        }
+    }
+
+    // append to 'div.quizzes' element
+
+    document.querySelector(".quizzes").innerHTML = phrases;
+}
+
+function appendWordsData(totalwords) {
+    console.log('appendWordsData()....');
+
+
+    var html = `
+<div class="tg-wrap">
+<table>
+<thead>
+<tr>
+<th class="target-head">الكلمة</th>
+<th class="mother-head">المعنى</th>
+<th class="audio-head">النطق</th>
+<th class="image-head">صورة</th>
+</tr>
+</thead>
+<tbody>`;
+    for (var i = 0; i < totalwords; i++) {
+        html += `
+<tr>
+<td class="target">${quizzes[0].alts[i].text}</td>
+<td class="mother">${quizzes[0].sols[i].text}</td>
+<td class="audio"><i class="fa fa-play"></i><audio preload="metadata"  src='${const_audio_portion +
+      quizzes[0].alts[i].key +
+      "?t=" +
+      quizzes[0].alts[i].audio_updated_at}' 
+  type='audio/mp3'>Your browser not supported</audio></td>
+<td class="image"><img src='${const_image_portion +
+      quizzes[0].alts[i].image}' /></td>
+</tr>  
+`;
+    }
+    html += `
+</tbody>
+</table>
+</div>
+`;
+    console.log('finish filling words table....');
+    document.querySelector(".content-wrapper").innerHTML = html;
+}
+
+function get_word_template_table() {
+    console.log("get_word_template_table()....");
+    console.log('lesson: ', lesson);
+    console.log('and quizzes object contains: ', quizzes);
+    // clean previous content
+    document.querySelector(".content-wrapper").innerHTML = "";
+
+    if ('alts' in quizzes[0]) {
+        var totalwords = quizzes[0].alts.length;
+        appendWordsData(totalwords);
+    }
+    appendPhrasesData();
+
+    // add event listner to all fa-play icons to play next audio
+    //+ element
+    document.querySelectorAll("td.audio,.cell.audio").forEach(function f(elem) {
+        elem.addEventListener("click", playAudio);
+    });
+} // end of get_word_template_table
+
+function playAudio(target) {
+    var elem = target.currentTarget;
+    var audioElem = elem.querySelector("audio");
+    console.log(audioElem);
+    audioElem.play();
+}
